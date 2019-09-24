fcff99e25ef96b1df6fd228245343dc33490ce22
Mario Vavti <mario@mariovavti.com>
2019-09-24 10:26:05 +0200

    sse: implement register

--- Zotlabs/Lib/Enotify.php
+++ Zotlabs/Lib/Enotify.php
@@ -973,4 +973,21 @@ class Enotify {
 		return $x;
 
 	}
+
+	static public function format_register($rr) {
+
+		$x = [
+			'notify_link' => z_root() . '/admin/accounts',
+			'name' => $rr['account_email'],
+			'addr' => $rr['account_email'],
+			'url' => '',
+			'photo' => z_root() . '/' . get_default_profile_photo(48),
+			'when' => datetime_convert('UTC', date_default_timezone_get(),$rr['account_created']),
+			'hclass' => ('notify-unseen'),
+			'message' => t('requires approval')
+		];
+
+		return $x;
+
+	}
 }
